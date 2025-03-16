12d420cb641f44ee4078ec25d838b6988021a9ba
cwt85 <cwt5278@gmail.com>
2025-03-16 15:50:40 +0800

    feat:Update test_solve_maze.py

--- DarkMaze/backend/tests/test_solve_maze.py
+++ DarkMaze/backend/tests/test_solve_maze.py
@@ -58,5 +58,19 @@ async def test_solver():
     await reset_request()
     for i in range(5):
         await move_request("down")
+    await move_request("right")
+    await move_request("down")
+    await move_request("right")
+    await move_request("right")
+    for i in range(4):
+        await move_request("up")
+    await move_request("right")
+    await move_request("right")
+    await move_request("down")
+    await move_request("right")
+    await move_request("right")
+    await move_request("down")
+    await move_request("right")
+    await move_request("down")
     #print(game_state)
     assert game_state["health"] == 666
