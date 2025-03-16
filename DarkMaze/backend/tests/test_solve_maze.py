6be770924a7a32124b6c1ad4f97e1911ded8ccb4
cwt85 <cwt5278@gmail.com>
2025-03-16 14:44:28 +0800

    feat:Update test_solve_maze.py

--- DarkMaze/backend/tests/test_solve_maze.py
+++ DarkMaze/backend/tests/test_solve_maze.py
@@ -19,6 +19,7 @@ async def login_request():
 
 async def reset_request():
     """Reset Game state"""
+    global game_state  # Add this line
 
     async with httpx.AsyncClient() as client:
         response = await client.get(RESET_URL)
@@ -29,6 +30,7 @@ async def reset_request():
 
 async def move_request(dir):
     """Simulates a frontend move request."""
+    global game_state  # Add this line
 
     payload = {"username": USERNAME, "direction": dir}
     
