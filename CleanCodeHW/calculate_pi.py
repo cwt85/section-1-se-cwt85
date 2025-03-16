973e7e7c03d229470ec3a0fb70de41a1f9622a6d
cwt85 <cwt5278@gmail.com>
2025-03-16 14:05:30 +0800

    refactor:Update calculate_pi.py

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -7,16 +7,16 @@ AREA_FACTOR = 4
 SQUARE_EXPONENT = 2
 POINT_COUNT_INCREMENT = 1
 
-INSIDE_CIRCLE = 0
+inside_circle = 0
 
 # Randomly generate points and count those inside the circle
 for i in range(NUM_POINTS):
     x = random.uniform(-RADIUS, RADIUS)
     y = random.uniform(-RADIUS, RADIUS)
-    if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT<= RADIUS**SQUARE_EXPONENT:
-        INSIDE_CIRCLE += POINT_COUNT_INCREMENT
+    if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
+        inside_circle += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-PI = (INSIDE_CIRCLE / NUM_POINTS) * AREA_FACTOR
+pi = (INSIDE_CIRCLE / NUM_POINTS) * AREA_FACTOR
 
-print(f"Estimated value of pi is: {PI}")
+print(f"Estimated value of pi is: {pi}")
