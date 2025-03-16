db13b940d508a2aaeb0f5db7ca1238b56b0306a2
cwt85 <cwt5278@gmail.com>
2025-03-16 14:41:46 +0800

    feat:Update test_game_over.py

--- DarkMaze/backend/tests/test_game_over.py
+++ DarkMaze/backend/tests/test_game_over.py
@@ -1,10 +1,11 @@
 from judge_code import game_over
 
 def test_game_over_0():
-    ...
+    return True
 
 def test_game_over_666():
-    ...
+    return True
 
 def test_game_over_other():
-    ...
+    return
+    
